05770c9b0f8471ec6fd0796cc09964dea8f8de5f
tonsh <tonshlee@gmail.com>
2013-03-02 02:01:56 +0800

    使用authenticated装饰器验证用户是否登录

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ import tornado.ioloop
 from handlers.HomeHandler import HomeHandler
 from handlers.HomeHandler import LoginHandler
 
+settings = {
+    "login_url": "/login",
+}
+
 application = tornado.web.Application([
     (r"/",      HomeHandler),
     (r"/login", LoginHandler),
-])
+], **settings)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
--- handlers/HomeHandler.py
+++ handlers/HomeHandler.py
@@ -5,12 +5,10 @@ import tornado.web
 
 class HomeHandler(tornado.web.RequestHandler):
     ''' 首页 '''
+    @tornado.web.authenticated
     def get(self):
-        # 如果用户未登录，跳转至登录页面
-        if not self.current_user:
-            self.redirect('/login')
-        else:
-            self.write('Hello Home')
+        ''' 如果用户未登录，跳转至登录页面 '''
+        self.write('Hello Home')
 
 
 class LoginHandler(tornado.web.RequestHandler):
